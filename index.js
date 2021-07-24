5a5a07e6ba8d2d2c4141f17752449c81d6fa9516
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-07-24 23:55:43 +0200

    [UPDATE]

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { Sequelize } = require('sequelize');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
 
 // npm run build in react project
@@ -21,5 +27,14 @@ app.get('/api', (req, res) => {
 
 
 app.listen(PORT, () => {
+    
+    try {
+        const sequelize = new Sequelize(process.env.DATABASE_URL);
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+    
     console.log(`server started on port ${PORT}`);
 });
