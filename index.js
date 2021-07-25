f5c88ef0225ec0a5555742245658b70f59a229b2
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-07-25 15:49:25 +0200

    [UPDATE]

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const bodyParser = require('body-parser');
 
@@ -27,7 +27,7 @@ app.get('/api', (req, res) => {
 
 
 app.listen(PORT, async () => {
-
+    // heroku config:set PGSSLMODE=no-verify
     try {
         const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
             dialectOptions: {
@@ -38,6 +38,23 @@ app.listen(PORT, async () => {
               }
         });
         await sequelize.authenticate();
+        // DIRTY TEST
+        const User = sequelize.define('User', {
+          // Model attributes are defined here
+          firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          lastName: {
+            type: DataTypes.STRING
+            // allowNull defaults to true
+          }
+        }, {
+          // Other model options go here
+        });
+        await sequelize.sync({force: true});
+
+        
         console.log('Connection has been established successfully.');
       } catch (error) {
         console.error('Unable to connect to the database:', error);
