bd06cbb0c42f931e6b4f1275dd7a6a0c24d7c83c
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-07-26 23:23:57 +0200

    [UPDATE]

--- dpm/src/App.js
+++ dpm/src/App.js
@@ -5,11 +5,21 @@ import { nanoid } from 'nanoid'
 import react, {useEffect, useState, useRef} from "react";
 
 function App() {
-  const [sharingUuid, setSharingUuid] = useState(nanoid(5));
+  const [sharingUuid, setSharingUuid] = useState("");
   
   let [filesUrls, setFilesUrl] = useState(new Map());
+  let [ws, setWs] = useState(new WebSocket("ws://localhost:5000"));
 
   useEffect( () => {
+
+    let parts = new URL(window.location).pathname.split("/")
+    if ( parts.length === 2 && parts[1] !== "") {
+      setSharingUuid(parts[1]);
+    } else {
+      let newUrl = `${window.location.origin}/${nanoid(5)}`;
+      window.location.href = `${newUrl}`;
+    }
+    
 
     window.addEventListener("drop", (event) => {
       event.preventDefault();
@@ -26,8 +36,24 @@ function App() {
     window.addEventListener("dragover", (event) => {
       event.preventDefault();
     });
+
+    ws.onmessage = msg => {
+      try {
+        const { data } = msg;
+        console.log(data);
+      } catch ( e ) {
+        console.log(e);
+      }
+    }
+    ws.onopen = ev => {
+      ws.send(JSON.stringify({
+        "topic":"incomming",
+        "hash": sharingUuid
+      }))
+    }
+
     
-  }, [filesUrls]);
+  }, [filesUrls, sharingUuid]);
 
 
   
@@ -37,7 +63,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       <div className="container">
-        <div style={{background: 'red'}}>
+        <p> 🤝 {sharingUuid} </p>
+        <div>
           <h1>Uplaod</h1>
         </div>
       </div>
@@ -45,9 +72,9 @@ function App() {
         Uploaded : 
         <code>{filesUrls.size}</code>
         { filesUrls && filesUrls.size > 0 && 
-          [...filesUrls.keys()].map( key => {
+          [...filesUrls.keys()].map( (key,i) => {
               return <div>
-                  <a href={filesUrls.get(key)} key={filesUrls.get(key)+key} download>{key}</a>
+                  <a href={filesUrls.get(key)} key={i} download>{key}</a>
                 </div>
           })
         }
